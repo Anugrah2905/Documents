d22c6af5d0034b861bc8aa077b7707a283259a7f
Anugrah2905 <137200341+Anugrah2905@users.noreply.github.com>
2023-06-25 22:25:53 -0700

    sets and tupple

--- t1.py
+++ t1.py
@@ -2,9 +2,9 @@ from turtle import*
 speed=('fastest')
 size=10
 angle=61
-colors =['red,pink,green,purple,blue,orange,violet,yellow']
+colors =['red','pink','green','purple','blue','orange','violet','yellow']
 while True:
-    pencolor(colors[size%6])
+    pencolor(colors[size%8])
     fd(size)
     lt(angle)
     size +=1
